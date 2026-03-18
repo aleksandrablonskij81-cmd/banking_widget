183fb2a8a3851e3cdeb27d08d2ebf5d850412e83
Александр <shura.yablonskij@mail.ru>
2026-03-18 17:27:15 +0300

    Final formatting and linting fixes

--- src/widget.py
+++ src/widget.py
@@ -2,8 +2,7 @@
 Модуль для маскировки счетов и карт.
 """
 
-
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info: str) -> str:
@@ -57,12 +56,11 @@ def get_date(date_str: str) -> str:
     year, month, day = date_part.split("-")
     return f"{day}.{month}.{year}"
 
+
 """тестовый блок"""
 
 if __name__ == "__main__":
-        print(mask_account_card("Visa Platinum 7000792289606361"))
-        print(mask_account_card("Счет 73654108430135874305"))
-        print(mask_account_card("Maestro 1596837868705199"))
-        print(get_date("2024-03-11T02:26:18.671407"))
-
-
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(get_date("2024-03-11T02:26:18.671407"))
